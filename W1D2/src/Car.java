8dfcfbcf3fc1b0e39a5e0b00bb4d8f9b55f23e2b
Teddysima <engtedo@yahoo.com>
2015-02-24 14:27:36 -0600

    x and y coordinat

---
+++ W1D2/src/Car.java
@@ -0,0 +1,51 @@
+
+public class Car {
+	private int x=0;
+	private int y=0;
+	
+	/*Car() {
+		this.x =0;
+		this.y = 0;
+	}
+	
+	Car(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}*/
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public void MoveUp(){
+		y += 1;
+	}
+	
+	public void MoveLeft(){
+		this.x += 1;
+	}
+	
+	public void MoveDown(){
+		this.y -= 1;
+	}
+	
+	public void MoveRight(){
+		this.x += 1;
+	}
+	public void PrintLocation(){
+		System.out.println("(" + this.x + "," + this.y + ")");
+		
+	}
+
+}
---
+++ W1D2/src/CarTestDrive.java
@@ -0,0 +1,12 @@
+
+public class CarTestDrive {
+	public static void main (String [] args){
+		Car myCar = new Car();
+		myCar.MoveDown();
+		myCar.MoveRight();
+		myCar.MoveUp();
+		myCar.MoveLeft();
+		myCar.PrintLocation();
+	}
+
+}
